551c350550e91208e2f3d661650ab517c49ea514
snpmyn <snpmyn@126.com>
2019-05-31 10:20:43 +0800

    Initial commit

---
+++ app/src/main/java/com/zsp/push/MainActivity.java
@@ -0,0 +1,19 @@
+package com.zsp.push;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @decs: 主页
+ * @author: 郑少鹏
+ * @date: 2019/5/31 10:19
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
